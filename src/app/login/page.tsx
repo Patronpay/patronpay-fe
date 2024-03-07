d4b639e2acbf510d2489ae37f2219c262f976cb9
jeevandza <jeevandza4@gmail.com>
2024-03-07 17:59:28 +0530

    test commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
   return (
     <div>
-      <div className="animate-shimmer text-gray-500 ">This is login page</div>
+      <div className="animate-shimmer text-gray-500 ">This is login page test</div>
       <input className="input-base"/>
     </div>
   );
