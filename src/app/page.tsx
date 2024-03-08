3fa6b16eaf2a902e5c2f915861be175a152bef38
jeevandpace <jeevan.d@pacewisdom.com>
2024-03-08 12:14:18 +0530

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <div className="">
-      <button className="btn-primary">test</button>
+      <button className="btn-primary">Test</button>
     </div>
   );
 }
